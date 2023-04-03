e561a96bfeb1962e8ba636de6cdc9c21a7cee753
EleaBahhadi <elea.bahhadi@ucalgary.ca>
2023-04-03 16:10:14 -0600

    Create Cage.java

---
+++ edu/ucalgary/oop/Cage.java
@@ -0,0 +1,47 @@
+package edu.ucalgary.oop;
+
+public class Cage {
+    private final Integer CAGEID;
+    private final String ANIMALSPECIES;
+    private int cleaningTime;
+
+    /**
+     * Constructor of Cage, sets all initial field values for the cage.
+     * @param cageID  An (Integer) ID of the cage.
+     * @param animalSpecies Species of the animal in the cage.
+     */
+    public Cage(Integer cageID, String animalSpecies) {
+        this.CAGEID = cageID;
+        this.ANIMALSPECIES = animalSpecies;
+
+        if (animalSpecies.equalsIgnoreCase("porcupine")) {
+            this.cleaningTime = 10;
+        } else {
+            this.cleaningTime = 5;
+        }
+    }
+
+    /**
+     * Gets the cleaning time of the cage.
+     * @return the cleaning time of the cage.
+     */
+    public int getCleaningTime() {
+        return cleaningTime;
+    }
+
+    /**
+     * Gets the species of the animal in the cage.
+     * @return the species of the animal in the cage.
+     */
+    public String getSpecies() {
+        return this.ANIMALSPECIES;
+    }
+
+    /**
+     * Gets the ID of the cage.
+     * @return the ID of the cage.
+     */
+    public Integer getCageID() {
+        return this.CAGEID;
+    }
+}
