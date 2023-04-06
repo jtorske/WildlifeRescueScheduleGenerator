389cf01af9779f534e5d5608e43fb1f478946d99
Jordan Torske <113051408+jtorske@users.noreply.github.com>
2023-04-05 21:58:12 -0600

    GUI Overhaul
    Big changes

--- edu/ucalgary/oop/GUI.java
+++ edu/ucalgary/oop/GUI.java
@@ -2,232 +2,205 @@ package edu.ucalgary.oop;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import javax.swing.table.TableModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.Vector;
 
 public class GUI {
-    private JFrame frame;
-    private JPanel panel;
-    private JLabel animalNicknameLabel, animalSpeciesLabel, taskLabel, startHourLabel;
-    private JTextField animalNicknameField, animalSpeciesField, taskField, startHourField;
-    private JButton submitButton;
+    private ConnectDatabase connectDatabase;
     private Connection connection;
     private Statement statement1, statement2;
     private ResultSet resultSet;
-    private JButton showValidValuesButton;
-    private JButton showTreatmentsButton;
-    private ConnectDatabase connectDatabase;
+
+    private JFrame mainFrame;
+    private JButton foodInputButton, cleaningInputButton, showButton;
 
     public GUI() {
-        frame = new JFrame("Animal Treatment Information");
-        frame.setSize(400, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 1));
-
-        animalNicknameLabel = new JLabel("Animal Nickname:");
-        animalNicknameField = new JTextField();
-        animalSpeciesLabel = new JLabel("Animal Species:");
-        animalSpeciesField = new JTextField();
-        taskLabel = new JLabel("Task:");
-        taskField = new JTextField();
-        startHourLabel = new JLabel("Start Hour:");
-        startHourField = new JTextField();
-
-        panel.add(animalNicknameLabel);
-        panel.add(animalNicknameField);
-        panel.add(animalSpeciesLabel);
-        panel.add(animalSpeciesField);
-        panel.add(taskLabel);
-        panel.add(taskField);
-        panel.add(startHourLabel);
-        panel.add(startHourField);
-
         try {
             connectDatabase = new ConnectDatabase();
             connectDatabase.createConnection();
-            connection = connectDatabase.getConnection(); // Get the Connection object from ConnectDatabase
+            connection = connectDatabase.getConnection();
             statement1 = connection.createStatement();
             statement2 = connection.createStatement();
             JOptionPane.showMessageDialog(null, "Successfully connected to the database");
+
+            createAndPopulateLogTable();
+
+            createAndShowGUI();
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Failed to connect to the database: " + ex.getMessage());
         }
 
-        submitButton = new JButton("Submit");
-        submitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String animalNickname = animalNicknameField.getText();
-                    String animalSpecies = animalSpeciesField.getText();
-                    String task = taskField.getText();
-                    int startHour = Integer.parseInt(startHourField.getText());
-
-                    resultSet = statement1.executeQuery("SELECT AnimalID FROM ANIMALS WHERE AnimalNickname = '"
-                            + animalNickname + "' AND AnimalSpecies = '" + animalSpecies + "'");
-                    if (!resultSet.next()) {
-                        JOptionPane.showMessageDialog(null, "No animal with the given nickname and species was found");
-                        return;
-                    }
-                    int animalID = resultSet.getInt("AnimalID");
-
-                    resultSet = statement2.executeQuery("SELECT TaskID FROM TASKS WHERE Description = '" + task + "'");
-                    if (!resultSet.next()) {
-                        JOptionPane.showMessageDialog(null, "The task does not exist");
-                        return;
-                    }
-                    int taskID = resultSet.getInt("TaskID");
-
-                    String sql = "INSERT INTO TREATMENTS (AnimalID, TaskID, StartHour) VALUES (?, ?, ?)";
-                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                    preparedStatement.setInt(1, animalID);
-                    preparedStatement.setInt(2, taskID);
-                    preparedStatement.setInt(3, startHour);
-                    preparedStatement.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Record added successfully");
-
-                    animalNicknameField.setText("");
-                    animalSpeciesField.setText("");
-                    taskField.setText("");
-                    startHourField.setText("");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, ex.getMessage());
-                }
-            }
-        });
-        showValidValuesButton = new JButton("Show Valid Values");
-        showValidValuesButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    JFrame validValuesFrame = new JFrame("Valid Values");
-                    validValuesFrame.setSize(400, 400);
-                    validValuesFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                    validValuesFrame.setLayout(new GridLayout(3, 1));
-
-                    DefaultListModel<String> nicknamesListModel = new DefaultListModel<>();
-                    DefaultListModel<String> speciesListModel = new DefaultListModel<>();
-                    DefaultListModel<String> tasksListModel = new DefaultListModel<>();
-
-                    resultSet = statement1.executeQuery("SELECT DISTINCT AnimalNickname FROM ANIMALS");
-                    while (resultSet.next()) {
-                        nicknamesListModel.addElement(resultSet.getString("AnimalNickname"));
-                    }
-
-                    resultSet = statement1.executeQuery("SELECT DISTINCT AnimalSpecies FROM ANIMALS");
-                    while (resultSet.next()) {
-                        speciesListModel.addElement(resultSet.getString("AnimalSpecies"));
-                    }
-
-                    resultSet = statement2.executeQuery("SELECT DISTINCT Description FROM TASKS");
-                    while (resultSet.next()) {
-                        tasksListModel.addElement(resultSet.getString("Description"));
-                    }
-
-                    JLabel nicknamesHeader = new JLabel("Animal Nicknames");
-                    JLabel speciesHeader = new JLabel("Animal Species");
-                    JLabel tasksHeader = new JLabel("Tasks");
-
-                    JList<String> nicknamesList = new JList<>(nicknamesListModel);
-                    JList<String> speciesList = new JList<>(speciesListModel);
-                    JList<String> tasksList = new JList<>(tasksListModel);
-
-                    validValuesFrame.add(nicknamesHeader);
-                    validValuesFrame.add(new JScrollPane(nicknamesList));
-                    validValuesFrame.add(speciesHeader);
-                    validValuesFrame.add(new JScrollPane(speciesList));
-                    validValuesFrame.add(tasksHeader);
-                    validValuesFrame.add(new JScrollPane(tasksList));
-
-                    validValuesFrame.setVisible(true);
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, ex.getMessage());
-                }
-
-            }
-        });
-
-        showTreatmentsButton = new JButton("Show Treatments");
-        showTreatmentsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                showTreatments();
-            }
-        });
-
-        panel.add(showTreatmentsButton);
-
-        panel.add(submitButton);
-        panel.add(showValidValuesButton);
-        frame.add(panel);
-        frame.setVisible(true);
     }
 
-    private void showTreatments() {
-        try {
-            JFrame treatmentsFrame = new JFrame("Treatments");
-            treatmentsFrame.setSize(600, 400);
-            treatmentsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    private void createAndShowGUI() {
+        mainFrame = new JFrame("Zoo Management");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(300, 200);
+        mainFrame.setLayout(new FlowLayout());
 
-            String[] columnNames = { "AnimalID", "TaskID", "StartHour" };
-            DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+        foodInputButton = new JButton("Food Input");
+        foodInputButton.addActionListener(new FoodInputListener());
+        mainFrame.add(foodInputButton);
 
-            resultSet = statement1.executeQuery("SELECT * FROM TREATMENTS");
-            while (resultSet.next()) {
-                int animalID = resultSet.getInt("AnimalID");
-                int taskID = resultSet.getInt("TaskID");
-                int startHour = resultSet.getInt("StartHour");
+        cleaningInputButton = new JButton("Cleaning Input");
+        cleaningInputButton.addActionListener(new CleaningInputListener());
+        mainFrame.add(cleaningInputButton);
 
-                Object[] rowData = { animalID, taskID, startHour };
-                model.addRow(rowData);
-            }
+        showButton = new JButton("Show");
+        showButton.addActionListener(new ShowListener());
+        mainFrame.add(showButton);
 
-            JTable treatmentsTable = new JTable(model);
-            treatmentsFrame.add(new JScrollPane(treatmentsTable));
+        mainFrame.setVisible(true);
+    }
 
-            treatmentsFrame.setVisible(true);
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
+    private class FoodInputListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            openInputWindow("Food Input");
         }
-        printSchedule();
     }
 
-    private void printSchedule() {
+    private class CleaningInputListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            openInputWindow("Cleaning Input");
+        }
+    }
+
+    private class ShowListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            displayLogTable();
+        }
+    }
+
+    private void displayLogTable() {
         try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            String currentDate = dateFormat.format(new Date());
-            System.out.println("Schedule for " + currentDate);
+            String queryLogTable = "SELECT * FROM log";
+            resultSet = statement1.executeQuery(queryLogTable);
 
-            resultSet = statement1
-                    .executeQuery("SELECT t.StartHour, ts.Description, a.AnimalNickname FROM TREATMENTS t " +
-                            "JOIN TASKS ts ON t.TaskID = ts.TaskID " +
-                            "JOIN ANIMALS a ON t.AnimalID = a.AnimalID " +
-                            "ORDER BY t.StartHour, ts.Description, a.AnimalNickname");
+            JTable logTable = new JTable(buildTableModel(resultSet));
 
-            int currentHour = -1;
-            while (resultSet.next()) {
-                int startHour = resultSet.getInt("StartHour");
-                String taskDescription = resultSet.getString("Description");
-                String animalNickname = resultSet.getString("AnimalNickname");
+            JFrame logTableFrame = new JFrame("Log Table");
+            logTableFrame.setSize(600, 300);
+            logTableFrame.add(new JScrollPane(logTable));
+            logTableFrame.setVisible(true);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Failed to display log table: " + ex.getMessage());
+        }
+    }
 
-                if (startHour != currentHour) {
-                    System.out.printf("%02d:00\n", startHour);
-                    currentHour = startHour;
-                }
+    private static TableModel buildTableModel(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        Vector<String> columnNames = new Vector<String>();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
 
-                System.out.println("* " + taskDescription + " (" + animalNickname + ")");
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (resultSet.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(resultSet.getObject(columnIndex));
             }
-        } catch (Exception ex) {
-            System.err.println("Error while printing the schedule: " + ex.getMessage());
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columnNames);
+    }
+
+    private void openInputWindow(String title) {
+        JFrame inputFrame = new JFrame(title);
+        inputFrame.setSize(300, 150);
+        inputFrame.setLayout(new GridLayout(3, 2));
+
+        JLabel idLabel = new JLabel("Animal Id:");
+        JTextField idField = new JTextField();
+        JLabel hourLabel = new JLabel("Hour:");
+        JTextField hourField = new JTextField();
+
+        inputFrame.add(idLabel);
+        inputFrame.add(idField);
+        inputFrame.add(hourLabel);
+        inputFrame.add(hourField);
+
+        JButton submitButton = new JButton("Submit");
+        inputFrame.add(submitButton);
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Implement the code to save the input to the database
+            }
+        });
+
+        inputFrame.setVisible(true);
+    }
+
+    private void createAndPopulateLogTable() throws SQLException {
+        // Check if the log table exists
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "log", null);
+        if (tables.next()) {
+            // The log table already exists, don't create a new one
+            return;
+        }
+
+        String createTableSQL = "CREATE TABLE log (" +
+                "Time VARCHAR(255), " +
+                "Task VARCHAR(255), " +
+                "Quantity VARCHAR(255), " +
+                "TimeSpent INTEGER, " +
+                "TimeAvailable INTEGER)";
+        statement1.execute(createTableSQL);
+
+        String queryTasksTreatments = "SELECT treatments.StartHour, tasks.Description, tasks.Duration " +
+                "FROM tasks " +
+                "INNER JOIN treatments ON tasks.TaskID = treatments.TaskID";
+        resultSet = statement1.executeQuery(queryTasksTreatments);
+
+        Map<String, List<LogEntry>> scheduleMap = new HashMap<>();
+
+        while (resultSet.next()) {
+            String startHour = resultSet.getString("StartHour");
+            String taskDescription = resultSet.getString("Description");
+            int duration = resultSet.getInt("Duration");
+
+            LogEntry logEntry = new LogEntry(taskDescription, "-", duration, 60 - duration);
+            scheduleMap.computeIfAbsent(startHour, k -> new ArrayList<>()).add(logEntry);
+        }
+
+        for (Map.Entry<String, List<LogEntry>> entry : scheduleMap.entrySet()) {
+            String startHour = entry.getKey();
+            List<LogEntry> logEntries = entry.getValue();
+            int timeSpent = logEntries.stream().mapToInt(LogEntry::getTimeSpent).sum();
+            int timeAvailable = 60 - timeSpent;
+
+            for (LogEntry logEntry : logEntries) {
+                String insertLogSQL = "INSERT INTO log (Time, Task, Quantity, TimeSpent, TimeAvailable) " +
+                        "VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(insertLogSQL);
+                preparedStatement.setString(1, startHour);
+                preparedStatement.setString(2, logEntry.getTask());
+                preparedStatement.setString(3, logEntry.getQuantity());
+                preparedStatement.setInt(4, logEntry.getTimeSpent());
+                preparedStatement.setInt(5, timeAvailable);
+
+                preparedStatement.executeUpdate();
+            }
         }
     }
 
     public static void main(String args[]) {
-        new GUI();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI();
+            }
+        });
     }
 }
