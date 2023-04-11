6b8f5310569b0307576fab48450ea11c8df0ed8a
EleaBahhadi <elea.bahhadi@ucalgary.ca>
2023-04-10 23:23:20 -0600

    merged test files

--- edu/ucalgary/oop/TestClass.java
+++ edu/ucalgary/oop/TestClass.java
@@ -2,7 +2,8 @@ package edu.ucalgary.oop;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +25,9 @@ public class TestClass {
     private ScheduledTask scheduledTask2;
     private ScheduledTask scheduledTask3;
 
+    private ConnectDatabase connectDB;
+    private ArrayList<MedicalTask> medicalTaskList;
+
     @Before
     public void setUp() {
         medicalTask1 = new MedicalTask(1, "X-Ray", 30, 60);
@@ -35,6 +39,14 @@ public class TestClass {
         treatment2 = new Treatment(2, 2, 2, 14);
         scheduledTask2 = new ScheduledTask(12, "Medical Checkup", TaskType.TREATMENT, "dog", "Joe, Bambi", 2, 40);
         scheduledTask3 = new ScheduledTask(15, "Cage Cleaning", TaskType.CLEANING, "coyote", "ALEX", 1, 30);
+
+        connectDB = new ConnectDatabase();
+        connectDB.createConnection();
+    }
+
+    @After
+    public void tearDown(){
+        connectDB.close();
     }
 
     @Test
@@ -274,4 +286,63 @@ public class TestClass {
                 () -> new ScheduledTask(10, "Feeding", TaskType.FEEDING, "dog", "Fido", -1, 20));
     }
 
+    
+    @Test
+    public void testSelectAnimals(){
+        ArrayList<Animal> animalList = connectDB.selectAnimals();
+        System.out.println(animalList.get(7).getAnimalNickname());
+        assertEquals(15, animalList.size());
+    }
+
+    @Test
+    public void testSelectTasks() {
+        this.medicalTaskList = connectDB.selectTasks();
+        assertEquals(10, medicalTaskList.size());
+    }
+
+
+    // @Test
+    // public void testInsertNewTask() throws IDExistException{
+    //     try {
+    //         Thread.sleep(100); // delay for 0.1 seconds
+    //     } catch (InterruptedException e) {
+    //         e.printStackTrace();
+    //     }
+    //     Integer testInsertID = 11;
+    //     connectDB.insertNewTask(testInsertID, "testing insert method", 10, 1, this.medicalTaskList);
+    //     try{
+    //         connectDB.getResults().close();
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    //     System.out.println("Please now remove the task with id of: " + testInsertID);
+    //     assertEquals(11, connectDB.selectTasks().size());
+    // }
+
+    // @Test
+    // public void testDeleteTask() throws IDNotExistException{
+    //     try {
+    //         Thread.sleep(500); // delay for 0.5 seconds
+    //     } catch (InterruptedException e) {
+    //         e.printStackTrace();
+    //     }
+    //     Integer testDeleteID = 11;
+    //     connectDB.deleteTask(testDeleteID, this.medicalTaskList);
+    //     try{
+    //         connectDB.getResults().close();
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    //     System.out.println("The tasks with ID of " + testDeleteID + " has been deleted.");
+    //     assertEquals(10, connectDB.selectTasks().size());
+    // }
+
+
+
+
+    @Test
+    public void testSelectTreatments(){
+        ArrayList<Treatment> TreatmentList = connectDB.selectTreatments();
+        assertEquals(30, TreatmentList.size());
+    }
 }
